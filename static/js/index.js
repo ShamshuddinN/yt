1c27ef19e909604ae0ccffba924ec6d34e916aaa
Shamshuddin Nadaf <nadafshamshu@gmail.com>
2024-08-04 20:54:15 +0530

    Updated with a bang!

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,6 @@ const HandleInput = (e) => {
     if (e.ctrlKey && e.key == 'Enter') {
         changeSlug(e.target.value)
         e.target.value = ''
-        
 }
 }
 
@@ -18,26 +17,36 @@ const changeSlug = (videoURL) => {
         let endIndx = videoURL.indexOf('?si=');
         let UsefulSlug = videoURL.slice(startIndx, endIndx)
         // console.log(UsefulSlug)
-        document.getElementsByTagName('iframe')[0].src = `https://www.youtube.com/embed/${UsefulSlug}`
+        document.getElementsByTagName('iframe')[0].src = `https://piped.video/watch?v=${UsefulSlug}`
+        ShowLink(`https://piped.video/watch?v=${UsefulSlug}`)
     }
     else if (videoURL.includes('watch?v=')) {
         let vList = videoURL.split('v=');
         // console.log(vList[1]);
-        document.getElementsByTagName('iframe')[0].src = `https://www.youtube.com/embed/${vList[1]}`
+        document.getElementsByTagName('iframe')[0].src = `https://piped.video/watch?v=${vList[1]}`
+        ShowLink(`https://piped.video/watch?v=${vList[1]}`)
     }
 
     document.getElementById('URLinput').value = ''
 }
 
+const ShowLink = (theLink) => {
+    let LinkEle = document.getElementById('linkElement')
+    let theAnchor = document.getElementById('LinkAnchor')
+    if (LinkEle.classList.value.includes('d-none')) {
+        LinkEle.classList.remove('d-none');
+    };
+    theAnchor.href = theLink
+}
 
 let windowWidth = window.innerWidth;
-let frameWidth = Math.floor(windowWidth * 0.6053)
+let frameWidth = Math.floor(windowWidth * 0.8053)
 
 if (windowWidth < 682) {
     frameWidth = Math.floor(windowWidth * 0.95)
 }
 
-let frameHeight = Math.round(frameWidth * 0.5629)
+let frameHeight = Math.round(frameWidth * 0.6)
 
 window.onload = function () {
     document.getElementsByTagName('iframe')[0].height = frameHeight;
